4a64972d02f71029d74f23de4ab7d0090dbd66b5
Алексей Мельников <alexmlnkv@gmail.com>
2023-01-20 17:29:37 +0300

    Vite Example VanillaTS, Pug

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+// Libs css
+import './libs/css/normalize.css';
+import '@fancyapps/ui/dist/fancybox.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import '@splidejs/splide/css';
+import './styles/style.scss';
+
+// Libs js
+import { Fancybox } from '@fancyapps/ui';
+import 'materialize-css/dist/js/materialize.min.js';
+//import { gsap } from 'gsap';
+import Splide from '@splidejs/splide';
+import './libs/js/imask.min.js';
+import './libs/js/lazyload.min.js';
+
+Fancybox.bind('[data-fancybox]', {
+  // Your options go here
+});
+new Splide('.splide').mount();
---
+++ vite.config.ts
@@ -0,0 +1,25 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
+import pugPlugin from 'vite-plugin-pug';
+
+interface Options {
+  pretty: boolean;
+}
+const options: Options = { pretty: true }; // FIXME: pug pretty is deprecated!
+const locals = { name: 'My Pug' };
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'src/pages/about.html'),
+      },
+    },
+  },
+  plugins: [pugPlugin(options, locals)],
+  server: {
+    open: '/index.html',
+    port: 5000,
+  },
+});
